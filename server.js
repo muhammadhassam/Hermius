474813d34ce7667f450311478fefb2c37af8da05
Alimahmood7 <alimahmmod02@gmail.com>
2016-10-20 22:48:01 -0700

    New Changes  in File

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var nodemailer = require('nodemailer');
 var multer  = require('multer');
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public')
+    cb(null, './public/images')
   },
   filename: function (req, file, cb) {
     cb(null,  Date.now()+file.originalname);
